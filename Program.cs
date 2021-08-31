7d190041c065deb519c364dfe2e27530525e7dc3
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-31 11:22:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLReaderWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
